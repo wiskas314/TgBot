704ffb70f5d0b754628fb58b32749c06b8a56533
wiskas314 <124578821+wiskas314@users.noreply.github.com>
2025-09-22 23:03:55 +0500

    Add files via upload
    Написал тест

---
+++ tgbot/src/main/java/com/example/demo/config/BotConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+
+@PropertySource("application.properties")
+public class BotConfig {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String token;
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+
+    public BotConfig(){
+
+    }
+}
