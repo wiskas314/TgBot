23eb0c2f20590a99b4f89bfa6eddc1991d7a98c0
wiskas314 <124578821+wiskas314@users.noreply.github.com>
2025-09-23 20:21:11 +0500

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.demo"},{"l":"com.example.demo.config"},{"l":"com.example.demo.controller"}];updateSearchResults();
