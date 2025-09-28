a5a9a6baca450da610f9c54c8a95411bc147aa79
Nikoly Belyaev <zaharovnikola@e1.ru>
2025-09-29 03:20:29 +0500

    Добавлены новые 2 класса для считывания сообщения и получения username Изменена логика метода handleUpdate класса MessageHandler, чтобы полностью избавиться от телеграмма Переписан тест под измененный класс MessageHandler Изменены параметры конструкотора класса TelegramBot Внесены небольшие изменения в метод onUpdateReceived

--- src/main/java/com/example/demo/controller/MessageHandler.java
+++ src/main/java/com/example/demo/controller/MessageHandler.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.Chat;
-import org.telegram.telegrambots.meta.api.objects.Update;
 
 /**
  * класс-обработчик сообщений Telegram бота
@@ -13,29 +11,30 @@ public class MessageHandler {
     /**
      * обрабатывает входящее обновление от Telegram  и возвращает текстовый ответ
      */
-    public String handleUpdate(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-
-            if (messageText.equals("/start")) {
-                Chat chat = update.getMessage().getChat();
-                String userName = (chat != null) ? chat.getFirstName() : "Неизвестный пользователь";
-                return sendStartMessage(userName);
-            } else if (messageText.equals("/help")) {
-                return sendHelpMessage();
+    public String handleUpdate(String message, String userName) {
+        if (!message.equals("")) {
+            if (message.equals("/start")) {
+                return startMessage(userName);
+            } else if (message.equals("/help")) {
+                return helpMessage();
             } else {
-                return echoMessage(messageText);
+                return echoMessage(message);
             }
         } else {
-            return "Ошибка обработки входных данных, проверьте что вы ввели текст!";
+            return ErrMessage();
         }
     }
 
-    /**
-     *формирует приветственное сообщение для команды /start
-     *содержит персональное приветствие с именем пользователя и информацию о доступных командах
-     */
-    private String sendStartMessage( String userName) {
+
+    private String ErrMessage() {
+        return "Ошибка обработки входных данных, проверьте что вы ввели текст!";
+    }
+
+    private String echoMessage(String messageText) {
+        return "Вы написали: " + messageText;
+    }
+
+    private String startMessage(String userName) {
         return "Привет, " + userName + "! Я бот, готовый помогать.\nЧтобы узнать, что я умею, введи /help";
     }
 
@@ -43,18 +42,10 @@ public class MessageHandler {
      * формирует справочное сообщение для команды /help
      * содержит список всех доступных команд с их описанием
      */
-    private String sendHelpMessage() {
+    private String helpMessage() {
         return """
-        Вот список доступных команд:
-        /start - Начать общение с ботом
-        /help - Получить список команд""";
-    }
-
-    /**
-     *обрабатывает обычные текстовые сообщения (не команды)
-     * возвращает эхо-ответ с текстом полученного сообщения
-     */
-    private String echoMessage(String messageText) {
-        return "Вы написали: " + messageText;
+                Вот список доступных команд:
+                /start - Начать общение с ботом
+                /help - Получить список команд""";
     }
 }
